9f6df234004379ce028d24f11263be5fb496f6f1
Filippo Alotta <alottafilippo@gmail.com>
2025-01-31 00:23:39 +0100

    added hero section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/Header'
+import Hero from '@/components/Hero'
 import React from 'react'
 
 const Homepage = () => {
   return (
     <>
         <Header />
+        <Hero />
     </>
   )
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import React from "react";
+import Profile from "@/public/images/profile.png";
+
+const Hero = () => {
+  const skills = [
+    "HTML",
+    "CSS",
+    "Sass",
+    "Tailwind",
+    "JavaScript",
+    "TypeScript",
+    "Angular",
+    "React",
+    "Next.js",
+    "Git",
+    "GitKraken",
+  ];
+
+  return (
+    <section className="flex flex-col-reverse md:flex-row gap-6 items-center justify-around px-7">
+      <div className="flex flex-col flex-1 text-center md:text-left justify-start items-center md:items-start gap-4 max-w-lg">
+        <h1 className="text-4xl md:text-5xl font-bold">Front-end Developer</h1>
+        <p className="max-w-96">
+          Hi, I&apos;m Filippo Alotta, a Front-end React Developer based in
+          Rome.
+        </p>
+        <div className="flex flex-wrap justify-center md:justify-start gap-2 items-center">
+          {skills.map((skill, index) => (
+            <span
+              key={index}
+              className="px-3 py-1 text-base border border-gray-500"
+            >
+              {skill}
+            </span>
+          ))}
+        </div>
+      </div>
+      <div className="flex">
+        <Image
+          src={Profile}
+          alt="profile image"
+          className="border-2 rounded-[40%_60%_30%_40%] border-black "
+          width={300}
+          height={300}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
